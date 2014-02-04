fe7c9d9225ece3bd0ae8c5e46b4268a8e3d2b3f7
shinson <sonia.hinson@gmail.com>
2014-02-03 23:53:33 -0500

    Update 50statesof.py

--- 50statesof.py
+++ 50statesof.py
@@ -1,4 +1,3 @@
-
 #List for states
 state_usa = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District Of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 
@@ -6,14 +5,14 @@ state_usa = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado
 state_abbrevs = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
 
 #Bring two lists together through zip, creating a 2 dimensional list
-zipped = zip(state_abbrevs, state_usa)
+zipped_state_abbrevs = zip(state_abbrevs, state_usa)
 
 #Begin printing first part of HTML, open select
 print '<select name="states">'
 
 #Begin for loop, to make html options for each state. Since list is 2D we have splice the loop to get value we want to print in each pair. 
 #[0] is for abbreviations, [1] is for the second value of the state
-for each_state in zipped:
+for each_state in zipped_state_abbrevs:
 	print "\t<option value='{0}'>{1}</option>".format(each_state[0], each_state[1])
 
 # Close HTML code
